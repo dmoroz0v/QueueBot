e0a4a634e73f7ec8434ff011757a2fc7bc851cb1
kramig <kramig@yandex-team.ru>
2024-11-09 21:13:23 +0500

    fix

--- make_cert_and_register_webhook.sh
+++ make_cert_and_register_webhook.sh
@@ -4,4 +4,4 @@ source .env
 
 (cd Cert; openssl req -newkey rsa:2048 -sha256 -nodes -keyout key.pem -x509 -days 3650 -out cert.pem -subj "/C=$CERT_COUNTRY/ST=$CERT_CITY/L=$CERT_CITY/O=$CERT_COMPANY/OU=$CERT_DEPARTMENT/CN=$BOT_DOMAIN")
 
-(cd Cert; curl https://api.telegram.org/bot$BOT_TOKEN/deleteWebhook)
+(cd Cert; curl -F "url=https://$BOT_DOMAIN:8443/webhook" -F "certificate=@cert.pem" https://api.telegram.org/bot$BOT_TOKEN/setWebhook)
